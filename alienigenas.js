4610febf2ec593ded7856d1ecc29440d5f53395a
unknown <filipebarbosa94@hotmail.com>
2020-10-16 11:57:55 -0300

    resolution-problem-alienigenas

---
+++ alienigenas.js
@@ -0,0 +1,30 @@
+/* divisão do problema.
+  ok passo 1 identificar quantas entradas possui o caso de teste
+  ok passo 2 identificar quantas letras distintas existem nesse caso de teste
+  ok passo 3 identificar quantas vezes cada letra se repete em uma unica entrada 
+  ok passo 4 comparar essas entradas e no fim do laço identificar quntos mDNA distintos possui cada entrada. */
+
+const testcase = [
+  [
+    ["AAA", "AAB", "ABC"],
+    ["AAAB", "AABA", "ABAA", "BAAA"],
+  ],
+];
+
+const indentifyDistintesMdna = (testCase) => {
+  for (const inputs of testCase) {
+    for (const mdna of inputs) {
+      const comparateSpecies = new Set();
+      for (const alienMdna of mdna) {
+        let convertesDateSpaciesIntoNumbers = 0;
+        for (const letter of alienMdna) {
+          convertesDateSpaciesIntoNumbers += letter.charCodeAt();
+        }
+        comparateSpecies.add(convertesDateSpaciesIntoNumbers);
+      }
+      console.log(comparateSpecies.size);
+    }
+  }
+};
+
+indentifyDistintesMdna(testcase);
