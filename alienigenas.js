8b007b241a41007c5269c8e778e5e7e03fcb434e
unknown <filipebarbosa94@hotmail.com>
2020-10-16 21:31:10 -0300

    correcion level de array

--- alienigenas.js
+++ alienigenas.js
@@ -4,16 +4,15 @@
   ok passo 3 identificar quantas vezes cada letra se repete em uma unica entrada 
   ok passo 4 comparar essas entradas e no fim do laço identificar quntos mDNA distintos possui cada entrada. */
 
-const testcase = [
+const testcase = 
   [
     ["AAA", "AAB", "ABC"],
     ["AAAB", "AABA", "ABAA", "BAAA"],
-  ],
-];
+  ];
+
 
 const indentifyDistintesMdna = (testCase) => {
-  for (const inputs of testCase) {
-    for (const mdna of inputs) {
+    for (const mdna of testCase) {
       const comparateSpecies = new Set();
       for (const alienMdna of mdna) {
         let convertesDateSpaciesIntoNumbers = 0;
@@ -21,10 +20,9 @@ const indentifyDistintesMdna = (testCase) => {
           convertesDateSpaciesIntoNumbers += letter.charCodeAt();
         }
         comparateSpecies.add(convertesDateSpaciesIntoNumbers);
-      }
+      } 
       console.log(comparateSpecies.size);
     }
-  }
+  
 };
-
 indentifyDistintesMdna(testcase);
